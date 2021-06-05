39e22861d6586dc34c04987f6c0a4dee2a70e843
Kimmo Lepola <kimmo.lepola@gmail.com>
2021-06-05 21:28:50 +0300

    get repositories, get statistics

--- src/App.js
+++ src/App.js
@@ -1,45 +1,120 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Typography, Card, Paper } from '@material-ui/core';
 
-import { useQuery, gql } from '@apollo/client';
+const asdf = async () => {
+  // repositories = await axios.get('https://api.github.com/orgs/Vincit/repos', { headers: { Authorization: 'Bearer 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' } });
+  // const repository = await axios.get('https://api.github.com/repos/Vincit/objection.js', { headers: { Authorization: 'Bearer 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' } });
+  // const statsContributors = await axios.get('https://api.github.com/repos/Vincit/objection.js/stats/contributors', { headers: { Authorization: 'Bearer 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' } });
+  // console.log('repositories: ', repositories);
+  // console.log('stats contributors', statsContributors);
+};
+asdf();
 
-const EXCHANGE_RATES = gql`
-query {
-  __schema {
-    types {
-      name
-      kind
-      description
-      fields {
-        name
+const organization = 'Vincit';
+
+const App = () => {
+  const [repositories, setRepositories] = useState([]);
+  const [statistics, setStatistics] = useState({});
+  const [retryList, setRetryList] = useState([]);
+  useEffect(async () => {
+    const result = await axios.get(`https://api.github.com/orgs/${organization}/repos`, { headers: { Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}` } });
+    setRepositories(result.data);
+  }, []);
+  useEffect(async () => {
+    const results = [];
+    /// /////////////////////
+    // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+    repositories.forEach((repo) => { // for of
+      const fetch = async () => {
+        const result = await axios.get(`https://api.github.com/repos/${organization}/${repo.name}/stats/contributors`, { headers: { Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}` } });
+        console.log(result);
+        if (result && result.status === 200) {
+          const trimmedResult = result.data.map((x) => ({
+            login: x.author ? x.author.login ? x.author.login : null : null,
+            total: x.total,
+            weeks: x.weeks.reduce((acc, cur) => {
+              if (cur.a && cur.c && cur.d) {
+                return acc.concat(cur);
+              }
+              return acc;
+            }, []),
+          }));
+          const newStatistics = { ...statistics };
+          newStatistics[repo.id] = trimmedResult;
+          setStatistics(newStatistics);
+          console.log('trimmed: ', trimmedResult);
+          sessionStorage.setItem(repo.id, JSON.stringify(trimmedResult));
+        }
+        if (result && result.status === 202) {
+          setRetryList([...retryList, repo]);
+        }
+      };
+      if (!sessionStorage.getItem(repo.id)) {
+        fetch();
       }
+    });
+  }, [repositories]);
+  console.log('reps: ', repositories);
+  console.log('statistics: ', statistics);
+  console.log('retryList: ', retryList);
+  console.log('session storage: ', sessionStorage);
+  // localStorage.clear();
+
+  const retryLoop = () => {
+    if (retryList.length) {
+      /// //////////////////
+      // https://stackoverflow.com/questions/37576685/using-async-await-with-a-foreach-loop
+      retryList.forEach((repo) => { // for of
+        const fetch = async () => {
+          const result = await axios.get(`https://api.github.com/repos/${organization}/${repo.name}/stats/contributors`, { headers: { Authorization: `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}` } });
+          console.log(result);
+          if (result && result.status === 200) {
+            const trimmedResult = result.data.map((x) => ({
+              login: x.author ? x.author.login ? x.author.login : null : null,
+              total: x.total,
+              weeks: x.weeks.reduce((acc, cur) => {
+                if (cur.a && cur.c && cur.d) {
+                  return acc.concat(cur);
+                }
+                return acc;
+              }, []),
+            }));
+            const newStatistics = { ...statistics };
+            newStatistics[repo.id] = trimmedResult;
+            setStatistics(newStatistics);
+            console.log('trimmed: ', trimmedResult);
+            sessionStorage.setItem(repo.id, JSON.stringify(trimmedResult));
+          }
+        };
+        fetch();
+      });
+      setTimeout(() => retryLoop(), 30000);
     }
-  }
-}
-`;
-
-function ExchangeRates() {
-  const { loading, error, data } = useQuery(EXCHANGE_RATES);
-
-  if (loading) return <p>Loading...</p>;
-  if (error) return <p>Error :(</p>;
-
-  console.log('data: ', data);
+  };
 
   return (
-    <div>
-      <p>
-        {JSON.stringify(data)}
-      </p>
+    <div style={{
+      display: 'flex', flexDirection: 'column', alignItems: 'center', background: 'yellow',
+    }}
+    >
+      {repositories.map((x) => (
+        <Paper
+          key={x.id}
+          style={{
+            display: 'flex', flexDirection: 'column', alignItems: 'center', margin: 5, padding: 50,
+          }}
+        >
+          <Typography variant="h6">{x.name}</Typography>
+          <Typography>{x.language}</Typography>
+          <Typography>Stargazers: {x.stargazers_count}</Typography>
+          <Typography>Forks: {x.fork_count}</Typography>
+          <Typography>Created at: {x.created_at}</Typography>
+          <Typography>Updated at: {x.updated_at}</Typography>
+        </Paper>
+      ))}
     </div>
   );
-}
-
-function App() {
-  return (
-    <div>
-      <ExchangeRates />
-    </div>
-  );
-}
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,38 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
-import { ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client';
-import { setContext } from '@apollo/client/link/context';
-import { ApolloProvider } from '@apollo/client/react';
 import App from './App';
-
-const httpLink = createHttpLink({
-  uri: 'https://api.github.com/graphql',
-});
-
-const authLink = setContext((_, { headers }) => {
-  // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('token');
-  // return the headers to the context so httpLink can read them
-  return {
-    headers: {
-      ...headers,
-      // authorization: token ? `Bearer ${process.env.REACT_APP_GITHUB_ACCESS_TOKEN}` : '',
-      authorization: 'Bearer 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',
-    },
-  };
-});
-
-const client = new ApolloClient({
-  link: authLink.concat(httpLink),
-  cache: new InMemoryCache(),
-});
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>,
+    <App />
   </React.StrictMode>,
   document.getElementById('root'),
 );
